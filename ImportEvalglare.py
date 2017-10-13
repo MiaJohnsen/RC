38d3ae4189d3b9b85160014a230f8159e6860c5d
MiaJohnsen <miaheekj@gmail.com>
2017-10-13 09:41:31 +0200

    Version 01

---
+++ ImportEvalglare.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 09:32:45 2017
+
+@author: mkj
+"""
+
+import os
+os.system("C:\\Radiance\\bin\\evalglare.exe -d C:\\users\\mkj\\desktop\\pic\\test.pic > C:\\users\\mkj\\desktop\\pic\\mia1234.txt")
