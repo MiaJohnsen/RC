6f16a44d6f71cdfbf5b616b90d3854e4c1c91048
MiaJohnsen <32192370+MiaJohnsen@users.noreply.github.com>
2017-11-01 10:41:23 +0100

    Add files via upload
    Added description

--- ImportEvalglare02.py
+++ ImportEvalglare02.py
@@ -3,7 +3,21 @@
 Created on Thu Oct 26 11:04:00 2017
 
 @author: mkj
+
+**Relative Contrast Program**
+- Getting images from Honeybee as .pic files, which covers a 360 degree angle 
+- Importing images into program in Python for further analysis
+    - Making sure that the users gives correct input
+        (Incoperating some sort of user-friendly error messages in case of wrong inputs)
+    - Execute evalglare.exe in order to find average luminance of each image
+    - Reading RGB values of each pixel and calculate corresponding luminance 
+    - Calculate standard deviation of luminance in each pixel and average luminance of image 
+    - Generete RC_(PI) 
+        - Plot with comparison of RC_(PI) and DGP 
+        - A plot for each image with colored pixels whose luminance values is higher than RC_(PI)
 """
+
+
 
 #Directions to folders containing .pic files / folder containing results / folder containing program
 imageDir = "C:\\users\\mkj\\Desktop\\Relativecontrast\\Pic\\"
@@ -44,13 +58,12 @@ f= open(folderDir +"txtFiles.txt","r")
 txtFiles= f.read().splitlines()
 f.close()
 
-for index in range(0,len(txtFiles)): 
+for index in range(0,len(txtFiles)):  
  g= open(destDir+txtFiles[index],"r")
  linesg= g.read().splitlines()
  g.close()
  
 #Creating .txt files for avg. values for each image
-
  k= open(averageDir+txtFiles[index]+".avg.txt","w") 
  lineLast= linesg[len(linesg)-1]
  items= lineLast.split(" ")
@@ -62,7 +75,6 @@ for index in range(0,len(txtFiles)):
  k.write(data+"\n")
 
 #Creating .txt files for glare sources for each image
-
  k.close()
  m= open(glareDir+txtFiles[index]+".glare.txt","w")
  for index2 in range(1,len(linesg)-1):
